7896719e243b5c077f4404af52b4efc92afd2622
Ernesto Medina <ernezto@gmail.com>
2015-12-10 18:16:12 -0500

    agregando gema de autenticacion

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.2.0'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
 
+gem 'devise'
+
 group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  #get 'welcome/index'
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
